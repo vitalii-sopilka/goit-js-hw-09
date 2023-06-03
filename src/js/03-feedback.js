9ab7095f8093a4845a19dacb9f3679e94eb96e57
Vitalii <33999219+vitalii-sopilka@users.noreply.github.com>
2023-06-03 10:23:10 +0200

    hw08-v08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,8 @@
 import throttle from "lodash.throttle";
 
 const form = document.querySelector('form');
-const email = document.querySelector('.feedback-form input');
-const message = document.querySelector('.feedback-form textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 
 form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onFormSubmit);
@@ -18,24 +16,28 @@ function onInput(e) {
 
 function onFormSubmit(e) {
     e.preventDefault();
-    console.log(formData);
 
-if (email.value === '' || message.value === '') {
-    return alert('Заповнені не всі поля!!!');
+    if (email.value === '' || message.value === '') {
+        return alert('Заповнені не всі поля!!!');
     };
-
+    console.log(formData);
+     {}
     e.currentTarget.reset();
 
     localStorage.removeItem(LOCALSTORAGE_KEY);
 };
 
 function getLocalStorageData () {
-    const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-
-    if (data) {
-        data.email ? email.value = data.email : "";
-        data.message ? message.value = data.message: "";
+    try {
+        const data = localStorage.getItem(LOCALSTORAGE_KEY);
+        if (!data) return
+        formData = JSON.parse(data)
+        Object.entries(([key, val]) => {
+            form.elements[key].value = val;
+        });
+    } catch (error) {
+        console.log(error.message)
     }
 };
 
-getLocalStorageData();
+window.addEventListener('load',getLocalStorageData);
